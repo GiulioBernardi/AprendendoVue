a11558944eadc4dff1ecddc2dddcfa272e362970
Giulio Bernardi <giulioccbernardi@gmail.com>
2022-07-11 11:25:48 -0300

    chamar api terceira

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      titulo: 'AluraPic',
+      fotos: []
+    }
+  },
+
+  created(){
+    let promise = this.$http.get('http://localhost:3000/v1/fotos')
+    promise
+      .then(res => res.json())
+      .then(fotos=> this.fotos = fotos, err => console.log(err));
+  }
+}
+</script>
+
+<style>
+
+  .corpo {
+    font-family: Helvetica, sans- serif;
+    width: 96%;
+    margin: 0, auto;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos{
+    list-style: none
+  }
+
+  .lista-fotos .lista-fotos-item{
+    display: inline-block
+  }
+
+</style>
