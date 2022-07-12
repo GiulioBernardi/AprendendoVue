7497db54232e3b894862d1bda2ae3d06fbee5dfb
Giulio Bernardi <giulioccbernardi@gmail.com>
2022-07-12 11:46:27 -0300

    adicionar titulo da imagem acima da mesma

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
     <h1 class="centralizado">{{ titulo }}</h1>
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo"/>
+        <div class="painel">
+          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+          <div class="painel-conteudo">
+            <img :src="foto.url" :alt="foto.titulo" />
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -11,40 +16,38 @@
 
 <script>
 export default {
-  data(){
-    return{
+  data() {
+    return {
       titulo: 'AluraPic',
       fotos: []
     }
   },
 
-  created(){
+  created() {
     let promise = this.$http.get('http://localhost:3000/v1/fotos')
     promise
       .then(res => res.json())
-      .then(fotos=> this.fotos = fotos, err => console.log(err));
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
 </script>
 
 <style>
+.corpo {
+  font-family: Helvetica, sans- serif;
+  width: 96%;
+  margin: 0, auto;
+}
 
-  .corpo {
-    font-family: Helvetica, sans- serif;
-    width: 96%;
-    margin: 0, auto;
-  }
+.centralizado {
+  text-align: center;
+}
 
-  .centralizado {
-    text-align: center;
-  }
+.lista-fotos {
+  list-style: none;
+}
 
-  .lista-fotos{
-    list-style: none;
-  }
-
-  .lista-fotos .lista-fotos-item{
-    display: inline-block;
-  }
-
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
 </style>
